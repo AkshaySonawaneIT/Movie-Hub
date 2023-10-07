ef1dbecea4272a03f146fafc87fd83fbeddcc61b
Akshay Sonawane <akshaysonawane1958@gmail.com>
2023-10-07 19:04:23 +0530

    Completed Hero Section

---
+++ src/pages/searchResult/SearchResult.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./searchResult.scss";
+
+function SearchResult() {
+    return (
+        <div>
+            SearchResult
+        </div>
+    )
+}
+
+export default SearchResult
