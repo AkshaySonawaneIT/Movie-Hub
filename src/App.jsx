09d8cb762fcee2d2f92c3d0f99100a211355de93
Akshay Sonawane <akshaysonawane1958@gmail.com>
2023-10-08 11:14:50 +0530

    Added popular, top rated, trending pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { fetchDataFromApi } from './utils/api.jsx';
 import { useSelector, useDispatch } from 'react-redux';
-import { getApiConfiguration } from './store/homeSlice.jsx';
+import { getApiConfiguration, getGenres } from './store/homeSlice.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './components/header/Header.jsx';
@@ -20,6 +20,7 @@ function App() {
 
   useEffect(() => {
     fetchApiConfig();
+    genresCall();
   }, [])
 
 
@@ -38,9 +39,28 @@ function App() {
       })
   }
 
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+  };
+
+
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/:mediaType/:id' element={<Details />} />
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import "./home.scss";
 import HeroBanner from './heroBanner/HeroBanner';
+import Trending from './trending/Trending';
+import Popular from './popular/Popular';
+import TopRated from './toprated/TopRated';
 
 function Home() {
     return (
         <div className='homePage'>
             <HeroBanner />
+            <Trending />
+            <Popular />
+            <TopRated />
         </div>
     )
 }
