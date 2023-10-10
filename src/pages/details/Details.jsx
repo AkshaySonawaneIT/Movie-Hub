2a245a0c7e47763075ca1f6f1221620632c4d2ce
Akshay Sonawane <akshaysonawane1958@gmail.com>
2023-10-10 07:46:12 +0530

    Added details page, explore and error components Project Finished

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,6 +4,9 @@ import useFetch from '../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
 import DetailsBanner from './detailsBanner/DetailsBanner';
 import Cast from './cast/Cast';
+import VideosSection from './videoSection/VideosSection';
+import Similar from './carousels/Similar';
+import Recommendation from './carousels/Recommendation';
 
 function Details() {
 
@@ -17,6 +20,9 @@ function Details() {
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
+      <Recommendation mediaType={mediaType} id={id} />
+      <Similar mediaType={mediaType} id={id} />
     </div>
   )
 }
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./error.scss";
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper';
+
 
 function Error() {
     return (
-        <div>
-            Error
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
         </div>
     )
 }
